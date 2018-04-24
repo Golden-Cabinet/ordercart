ef10ca2d34a276922f065982518011da8bc99607
matt webb <mwebb@ethoscodelabs.com>
2018-04-24 06:49:07 -0700

    [wip] formulas add

--- resources/views/dashboard/formulas/create.blade.php
+++ resources/views/dashboard/formulas/create.blade.php
@@ -24,36 +24,49 @@
                     </tr>                
                     <tbody id="ingRows"></tbody>
                 </table>
+                <div class="col-md-8 float-right" style="border-top: 1px solid black; clear: both">
+                        <table class="table" id="ingredientsTotal">
+                            <tr>
+                                <td style="width: 40%">Total</td>
+                                <td style="width: 25%">Grams</td>
+                                <tdth style="width: 20%">&nbsp;</td>
+                                <td style="10%">&nbsp;</td>
+                                <td style="width: 5%">$<span id="runningTotal">0.00</span><td>
+                            </tr>                
+                        </table>
+                </div>    
+                
                 <hr />                
             </div>            
-            {{ csrf_field() }}
-            <hr />
-            <div class="finalizeFormula" style="display: none">
-                <h5>Create Your Formula Name and Save It!</h5>
+            {{ csrf_field() }}            
+        
+        </div>
+        
+        <div style="width: 100%">
+            <div class="col-md-7 float-left">
+                    <h5>Search For Ingredients</h5>
+                    <input id="ingredientsAuto" class="form-control mb-4" tabindex="0" placeholder="Start Your Search Here">  
+            </div>
+            
+            <div class="col-md-3 float-left">
+                    <h5>Grams</h5>
+                <input type="number" step="0.1" onkeydown="limit(this);" onkeyup="limit(this);" min="0" max="99" id="initialgrams" class="form-control">
+            </div>
+
+            <div class="col-md-2  float-left mt-2 mb-4">
+                <p class="text-right"><a href="#" id="addToTopRow" class="btn btn-info mt-4 text-white" data-ingredientId="" data-ingredient="" data-cpg="">Add To Formula</a></p>            
+            </div>            
+        </div>
+
+        <div style="clear: both; width: 100%"><span id="calculateFormula" style="display:none;" class="btn btn-info text-center float-right"><i class="fas fa-calculator"></i> Finalize Formula</span></div>
+        <hr style="clear: both; width: 100%" />
+        <div class="finalizeFormula" style="display: none; width: 100%; clear: both;">
+                
+            <h5>Create Your Formula Name and Save It!</h5>
             <input type="text" style="background: #ffffe0" class="form-control mb-4" id="formulaName" name="formula_name" placeholder="Enter Your New Formula Name">
             <div class="col-md-6 float-left text-bold" style="font-size: 1.2rem;"><p>Grand Total: $<span id="grandTotal">0.00</span></p> </div> 
-                <button href="#" id="saveFormula" type="submit" class="btn btn-success btn-lg text-center float-right"><i class="fas fa-check"></i> Save Formula</button>
-           
+            <button href="#" id="saveFormula" type="submit" class="btn btn-success btn-lg text-center float-right"><i class="fas fa-check"></i> Save Formula</button>    
         </div>
-        </div>
-        
-        <div class="col-md-5 float-left">
-                <h5>Search For Ingredients</h5>
-                <input id="ingredientsAuto" class="form-control mb-4" tabindex="0" placeholder="Start Your Search Here">  
-        </div>
-        
-        <div class="col-md-4 float-left">
-                <h5>Grams</h5>
-            <input type="number" onkeydown="limit(this);" onkeyup="limit(this);" min="0" max="99" id="initialgrams" class="form-control">
-        </div>
-
-        <div class="col-md-3  float-left mt-2">
-            <a href=""id="addToTopRow" class="btn btn-info mt-4 text-white">Add To Formula</a>
-            
-        </div>
-        <div style="clear: both"><a href="#" id="calculateFormula" style="display:none;" class="btn btn-info text-center float-right"><i class="fas fa-calculator"></i> Finalize Formula</a></div>
-
-
                       
         </form>
         </div>
@@ -120,30 +133,61 @@
                     $.ajax({
                         url : '/dashboard/formulas/search/product/'+ product +'/brand/'+ brand,
                         type: 'GET',
-                        success : function(data){
-                            $("#ingredientsAuto").val('');
-                            $('#stepTwo').slideDown( "slow", function() {
-                                $( "#newFormula" ).fadeIn(1300);
-                              });                            
-                            var productDetails = data['ingredient'][0];
+                        success : function(data){   
+                            $("#initialgrams").focus();                            
 
-                            //check for existing row to prevent dupe
-                            if($("#row_"+ productDetails['id'] +"").length){
-                                $('#dupeModal').modal();                                
-                            } else {                                
-                                var addRow = '<tr id="row_'+ productDetails["id"] +'"><td>'+ productDetails["pinyin"] +'</td><td><input type="number" onkeydown="limit(this);" onkeyup="limit(this);" min="0" max="99" data-cpg="'+ productDetails["costPerGram"] +'" data-prid="'+ productDetails["id"] +'" autofocus class="userGrams form-control" style="max-width: 100%" step="0.1" id="userGram_'+ productDetails["id"] +'" name="userGram_'+ productDetails["id"] +'"></td><td>$'+ productDetails["costPerGram"] +'</td><td>$<span class="subTotals" id="subTotal_'+ productDetails["id"] +'">0.00</span></td><td><a href="#" tabindex="-1" class="removeIngredient btn btn-sm btn-danger text-white">Remove</a></td></tr>';
-                                $('#ingredientslist > tbody:last').append(addRow);
-                                // if we have any existing rows, we always wait until the new one has got a value before moving on
-                                if($('#ingRows > tr').length > 0){
-                                    $('#calculateFormula').hide();
-                                }
-                            }
-                            
-                            $("#userGram_"+ productDetails['id'] +"").focus();                           
+                            // add data fields to button
+                            var productDetails = data['ingredient'][0];
+                            var productName= product;
+                            var productId = productDetails['id'];
+                            var productCostPerGram = productDetails['costPerGram'];
+                            $("#addToTopRow").attr('data-ingredientId',productId);
+                            $("#addToTopRow").attr('data-ingredient',productName);
+                            $("#addToTopRow").attr('data-cpg',productCostPerGram);
+
                         }
                     })                    
                 }
-            });          
+            });
+
+        // add ingredient button stuff
+        $("#addToTopRow").on('click keydown', function(){
+            var currentGrams = $('#initialgrams').val();
+            $("#ingredientsAuto").val('');
+            $('#initialgrams').val('');
+            $('#stepTwo').slideDown( "slow", function() {
+                $( "#newFormula" ).fadeIn(1300);
+                $("#calculateFormula").show();
+            });                           
+            
+            var productId = $(this).attr('data-ingredientId');
+            var productName = $(this).attr('data-ingredient');
+            var productCostPerGram = $(this).attr('data-cpg');
+            var subTotal = productCostPerGram * currentGrams; 
+            var ingredientSubTotal = parseFloat(subTotal).toFixed(2);
+            var currentGrandTotal = $('#runningTotal').text();
+            var newGrandTotal = parseFloat(currentGrandTotal) + parseFloat(subTotal);
+            
+            var addedTotal = newGrandTotal;
+            $('#runningTotal').html(parseFloat(newGrandTotal).toFixed(2));            
+
+            //check for existing row to prevent dupe
+            if($("#row_"+productId +"").length){
+                $('#dupeModal').modal();                                
+            } else {                
+                var addRow = '<tr id="row_'+ productId +'"><td>'+ productName +'</td><td><input type="number" onkeydown="limit(this);" onkeyup="limit(this);" min="0" max="99" data-cpg="'+ productCostPerGram +'" data-prid="'+ productId +'" autofocus class="userGrams form-control" style="max-width: 100%" step="0.1" id="userGram_'+ productId +'" name="userGram_'+ productId +'" value="'+currentGrams+'"></td><td>$'+ productCostPerGram +'</td><td class="subs">$<span class="subTotals" id="subTotal_'+ productId +'">'+ingredientSubTotal+'</span></td><td><a href="#" tabindex="-1" class="removeIngredient btn btn-sm btn-danger text-white">Remove</a></td></tr>';
+                $('#ingredientslist > tbody:last').append(addRow);
+                // if we have any existing rows, we always wait until the new one has got a value before moving on
+                $("#addToTopRow").attr('data-ingredientId','');
+                $("#addToTopRow").attr('data-ingredient','');
+                $("#addToTopRow").attr('data-cpg','');
+                if($('#ingRows > tr').length > 0){
+                    $('#calculateFormula').hide();
+                }
+                $("#userGram_"+ productId +"").focus();
+            }
+            
+        })    
         
         // address weird shift+tab issue to get back into grams field
         $( document ).ready(function() {
@@ -162,19 +206,36 @@
             if($('.userGrams').length){
                 $('#calculateFormula').show(); 
             }
-                       
-            var currentSubTotal = $("#subTotal_"+ prid +"").text();  
+                        
+            var currentSubTotal = $("#subTotal_"+ prid +"").text();            
             var grams = $(this);
             var prid = $(grams).data('prid');
             var cpg = $(grams).data('cpg');
             var userGrams = $(grams).val();            
             var subTotal = parseFloat(userGrams) * parseFloat(cpg);             
             var ingredientSubTotal = parseFloat(subTotal).toFixed(2);
-            var grandTotal = '0.00';            
+            var currentGrandTotal = $('#runningTotal').text();
+            var parsedGrandTotal = parseFloat(currentGrandTotal);
+            
+            update_amounts();
+       
 
             $("#subTotal_"+ prid +"").html(ingredientSubTotal);
             
         });
+
+        // dynamically adjust the grand total
+        function update_amounts()
+            {
+                var sum = 0;
+                $('.subTotals').each(function () {
+                    var prodprice = Number($(this).text());
+                    console.log(prodprice);
+                    sum = sum + prodprice;
+                });
+
+                $("#runningTotal").text(sum.toFixed(2));
+            }
 
         function limit(element)
         {
@@ -191,8 +252,9 @@
             }
         }
           
-        /** FORMULA OVERVIEW - REMOVE ADDED INGREDIENT **/
+        /** FORMULA OVERVIEW - REMOVE ADDED INGREDIENT  **/
             $(document).on('click','.removeIngredient', function() {
+                 
                 var remove = $(this);
                 var rprid = $(this).data('prid');
                 
@@ -209,11 +271,12 @@
                         $( "#newFormula" ).fadeOut(1300);
                       });
                     $("#ingredientsAuto").focus();
-                }  
+                }
+                update_amounts();  
                 return false;
             });
             
-         /** FORMULA CALCULATION **/ 
+         /** FORMULA CALCULATION **/
          $('#calculateFormula').on('click',function(){
 
             $("#formulaName").focus();
@@ -243,7 +306,6 @@
                 $('#saveFormula').prop('disabled',false);
              }           
          });
-         
             
         </script>       
     @endpush
